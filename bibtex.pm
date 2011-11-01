91460c2830ba89655426bcab8899bdf01e365fc4
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-01 10:13:10 +0100

    initial

---
+++ bibtex.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+package IkiWiki::Plugin::bibtex;
+use warnings;
+use strict;
+use IkiWiki 3.00;
+
+sub import {
+    hook(type => "preprocess", id => "bibtex", call => \&bibtex_preprocess);
+}
+
+
+sub preprocess {
+    my %params=@_;
+    return 1;
+}
+
