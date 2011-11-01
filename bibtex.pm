708e4154f7fb8e69a025ef60e9c33f5f7698558e
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-01 20:10:51 +0100

    first version

--- bibtex.pm
+++ bibtex.pm
@@ -4,6 +4,9 @@ use warnings;
 use strict;
 use IkiWiki 3.00;
 
+
+my $bibtex_file = "/home/ihrke/work/bib/dummy.bib";
+
 sub import {
     hook(type => "preprocess", id => "bibtex", call => \&bibtex_preprocess);
 }
@@ -11,7 +14,27 @@ sub import {
 
 sub bibtex_preprocess {
     my %params=@_;
-    return 1;
+	 print ">> bibtex.pm: $bibtex_file\n";
+	 print ">> bibtex.pm: parameters '" . %params . "'\n";
+
+	 my $k;
+	 my $v;
+	 while ( ($k,$v) = each %params ) { print "$k => $v\n"; }
+    open(my $in,  "<",  $bibtex_file )  or die "Can't open $bibtex_file: $!";
+	 my @lines = <$in>;
+	 close $in or die "$in: $!";
+
+	 my $content = join("",@lines);
+#	 print $content;
+
+	 my $key=$params{"key"};
+	 my $entry="";
+	 print "key=$key\n";
+	 if ($content =~ /(@\w+{\s*$key.*?}(?=\s*@))/s ){
+		  $entry=$1;
+	 }
+		
+    return "<pre id='bibtex'>".$entry."</pre>";
 }
 
 
