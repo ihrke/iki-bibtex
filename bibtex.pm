288b12f5491fc2a95d3a5546a45a09bbe28e9f76
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-01 21:31:10 +0100

    switched to Text::Bibtex

--- bibtex.pm
+++ bibtex.pm
@@ -4,37 +4,71 @@ use warnings;
 use strict;
 use IkiWiki 3.00;
 
-
-my $bibtex_file = "/home/ihrke/work/bib/dummy.bib";
+use Text::BibTeX;
 
 sub import {
+    hook(type => "getsetup", id => "bibtex", call => \&getsetup);
     hook(type => "preprocess", id => "bibtex", call => \&bibtex_preprocess);
+
 }
 
-
+sub getsetup () {
+    return 
+		  plugin => {
+            description => "Include bibtex in ikiwiki",
+            safe => 1,
+            rebuild => 1,
+            section => "misc",
+	 },
+	 bibtex_file => {
+		  type => "string",
+		  safe=>1,
+		  description=> "bibtex-file to use",
+		  example => "/home/user/test.bib",
+		  rebuild => 1,
+	 },
+}
 sub bibtex_preprocess {
     my %params=@_;
-	 print ">> bibtex.pm: $bibtex_file\n";
-	 print ">> bibtex.pm: parameters '" . %params . "'\n";
+	 my $bibtex_file="";
+	 if( exists $config{bibtex_file} ){
+		  $bibtex_file=$config{bibtex_file};
+	 } 
 
-	 my $k;
-	 my $v;
-	 while ( ($k,$v) = each %params ) { print "$k => $v\n"; }
-    open(my $in,  "<",  $bibtex_file )  or die "Can't open $bibtex_file: $!";
-	 my @lines = <$in>;
-	 close $in or die "$in: $!";
-
-	 my $content = join("",@lines);
-#	 print $content;
-
-	 my $key=$params{"key"};
-	 my $entry="";
-	 print "key=$key\n";
-	 if ($content =~ /(@\w+{\s*$key.*?}(?=\s*@))/s ){
-		  $entry=$1;
+	 # overwrite with file parameter
+	 if( exists $params{file} ){
+		  $bibtex_file=$params{file};
 	 }
-		
-    return "<pre id='bibtex'>".$entry."</pre>";
+	 unless( -e $bibtex_file ){
+		  error("bibtex-file $bibtex_file doesn't exist\n");
+	 }
+	 my $key;
+	 if( exists $params{key} ){
+		  $key=$params{key};
+	 } else {
+		  error( "no key given");
+	 }
+
+	 debug(">> bibtex.pm: $bibtex_file\n");
+	 debug(">> bibtex.pm: $key\n");
+
+	 my $bibfile = new Text::BibTeX::File $bibtex_file;
+	 my $entry;
+	 my $found;
+	 while ($entry = new Text::BibTeX::Entry $bibfile)
+	 {
+		  next unless $entry->parse_ok;
+		  
+		  if ( $entry->key eq $key ){
+				$found=$entry;
+				print $entry->print_s;
+		  }
+   }
+	 unless(defined $found){
+		  error("key $key not in file $bibtex_file\n");
+	 }
+
+    return "[[!toggle id='$key' text='$key']]".$found->print_s ."";
 }
 
 
