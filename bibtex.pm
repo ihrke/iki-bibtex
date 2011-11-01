ed6d4809d8efb033494ed21eec5209d78b56deaa
Matthias Ihrke <ihrke@nld.ds.mpg.de>
2011-11-01 22:16:32 +0100

    structure for choice of format

--- bibtex.pm
+++ bibtex.pm
@@ -5,6 +5,7 @@ use strict;
 use IkiWiki 3.00;
 
 use Text::BibTeX;
+use Text::Format;
 
 sub import {
     hook(type => "getsetup", id => "bibtex", call => \&getsetup);
@@ -27,17 +28,31 @@ sub getsetup () {
 		  example => "/home/user/test.bib",
 		  rebuild => 1,
 	 },
+	 bibtex_default_output_format =>{
+		  type => "string",
+		  safe=>1,
+		  description=> "output format for the bibtex-entry",
+		  example => "raw,citation",
+		  rebuild => 1,
+	 },
 }
 sub bibtex_preprocess {
     my %params=@_;
 	 my $bibtex_file="";
+	 my $output_format="raw";
 	 if( exists $config{bibtex_file} ){
 		  $bibtex_file=$config{bibtex_file};
+	 } 
+	 if( exists $config{bibtex_default_output_format} ){
+		  $output_format=$config{bibtex_default_output_format};
 	 } 
 
 	 # overwrite with file parameter
 	 if( exists $params{file} ){
 		  $bibtex_file=$params{file};
+	 }
+	 if( exists $params{format} ){
+		  $output_format=$params{format};
 	 }
 	 unless( -e $bibtex_file ){
 		  error("bibtex-file $bibtex_file doesn't exist\n");
@@ -68,7 +83,17 @@ sub bibtex_preprocess {
 		  error("key $key not in file $bibtex_file\n");
 	 }
 
-    return "[[!toggle id='$key' text='$key']]".$found->print_s ."";
+	 my $output="";
+	 if( $output_format eq "citation"){
+
+	 } else {
+		  $output="<div id='bibtex_entry'>".$found->print_s."</div>";
+	 }
+
+	 
+#	 my $formatter=Text::Format->new( );
+#    return "<pre id='bibtex'>".$formatter->format($found->print_s) ."</pre>";
+	 return $output;
 }
 
 
